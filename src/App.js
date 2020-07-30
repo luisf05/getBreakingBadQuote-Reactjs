bdbfbed25e691d690b1ad31c146d47d91f88a40b
“luisf05” <luisf0599@gmail.com>
2020-07-30 09:23:49 -0600

    example create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Quote from "./components/Quote";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 1rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8 ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 function App() {
+  //quote State
+  const [quote, saveQuote] = useState({});
+
+  const requestAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const quote = await api.json();
+    saveQuote(quote[0]);
+  };
+
+  //Load quote
+  useEffect(() => {
+    requestAPI();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Quote quote={quote} />
+      <Button onClick={requestAPI}>Get a quote </Button>
+    </Container>
   );
 }
 
